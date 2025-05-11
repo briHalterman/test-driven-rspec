239ce9e3711400d59876550b15064871df85f40a
Brittany Halterman <halterma@gmail.com>
2025-05-11 16:54:23 -0400

    Remove test-rspec

--- episode-04/app/models/console.rb
+++ episode-04/app/models/console.rb
@@ -2,7 +2,7 @@ class Console < ActiveRecord::Base
   validates :name, presence: true
   validates :manufacturer, presence: true
 
-  scope :nintendo, -> { where(manufacturer: "Nintendo") }
+  scope :nintendo, -> { where(manufacturer: 'Nintendo') }
 
   def formatted_name
     "#{manufacturer} #{name}"
